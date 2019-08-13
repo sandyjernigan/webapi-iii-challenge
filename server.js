c9309d5e5ed50af77d8a36cbb0883b30f6fd9817
Sandy <sandy.jernigan@gmail.com>
2019-08-12 22:37:22 -0400

    Setting up

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  // TODO: logger()
+  // logger logs to the console the following information about each request: request method, request url, and a timestamp
+  // this middleware runs on every request made to the API
+
 
 };
 
